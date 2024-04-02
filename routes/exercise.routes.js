d41818e1db798e94a3dcfe1069108915343fb783
Guilherme Carvalho <guicarv99@gmail.com>
2024-04-02 19:54:37 +0100

    Creating anew exercise is working

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const Exercise = require('../models/Exercise.model');
+const Workout = require('../models/Workout.model');
+
+//? ------------ ROUTE TO CREATE A NEW EXERCISE ON THE WORKOUT ------------ //
 
 router.post('/exercise', async (req, res, next) => {
   const { name, sets, reps, weight, notes, duration, workout } = req.body;
@@ -15,10 +18,20 @@ router.post('/exercise', async (req, res, next) => {
       workout,
     });
 
+    const updatedWorkout = await Workout.findByIdAndUpdate(workout, {
+      $push: { exercises: newExercise._id },
+    });
+
+    if (!updatedWorkout) {
+      return res.status(404).json({ message: 'Workout not found' });
+    }
+
     res.status(201).json(newExercise);
   } catch (error) {
     next(error);
   }
 });
+
+//? ------------ ROUTE TO GET ALL OF THE EXERCISES FROM THE WORKOUT ------------ //
 
 module.exports = router;
